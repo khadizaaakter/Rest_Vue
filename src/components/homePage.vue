68d0b010d24c1b1925d1e3c854935c4b434cbcdb
Khadiza Akter <khadizaakter44555@gmail.com>
2025-12-17 17:41:09 +0600

    dfff

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -1,18 +1,34 @@
 <template>
   <HeaderMain />
-  <div><h2>Home</h2></div>
+  <div>
+    <h2>{{ restData }}</h2>
+  </div>
 </template>
 <script setup>
 import { onMounted } from "vue";
 import { useRouter } from "vue-router";
 import HeaderMain from "./layouts/header-main.vue";
+import axios from "axios";
+import { ref } from "vue";
 const router = useRouter();
+
+const restData = ref([]);
+
+const fetchRestaurant = async () => {
+  try {
+    const res = await axios.get(`http://localhost:3000/resaurant`);
+    restData.value = res.data;
+  } catch (error) {
+    console.error("Error fetching restaurant data:", error);
+  }
+};
 
 onMounted(() => {
   const user = localStorage.getItem("userData");
   if (!user) {
     router.push("/");
   }
+  fetchRestaurant();
 });
 </script>
 <style scoped>
